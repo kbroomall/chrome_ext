69872436e083ad44547dee71ae3826d350b2a303
kbroomall <kevin.broomall@gmail.com>
2017-07-23 12:11:57 -0400

    fixed css
    fixed css so the GUID appears in the center of the toolbar cross platforms.

--- smtlid2/toolbar.js
+++ smtlid2/toolbar.js
@@ -2,5 +2,5 @@ console.log("toolbar.js");
 
 
 chrome.runtime.onMessage.addListener(function(response, sender, sendResponse){
-  document.write("<style>body{color:orange;overflow-y:hidden;margin: 11px 0 0 22px;font-family: 'Ubuntu Mono', monospace;}</style>" + "<div id='content2342422'>" + response + "</div>");
+  document.write("<style>body{color:orange;margin:12px;overflow:hidden;font-family: 'Ubuntu Mono', monospace;}#content2342422{text-align:center;vertical-align:middle;}</style>" + "<div id='content2342422'>" + response + "</div>");
 });
